64022061016d33d7e6808559b1f920347e14f40c
Joao Ross <2024011504@svs.iffarroupilha.local>
2026-03-24 21:23:44 -0300

    Alteração de false para true nas requests

--- app/Http/Requests/UpdateBancaRequest.php
+++ app/Http/Requests/UpdateBancaRequest.php
@@ -11,7 +11,7 @@ class UpdateBancaRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateTccRequest.php
+++ app/Http/Requests/UpdateTccRequest.php
@@ -11,7 +11,7 @@ class UpdateTccRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
