a90a1117f61f307bb025e2551a84e9ce5157aa11
Joao Ross <2024011504@svs.iffarroupilha.local>
2026-03-24 21:12:56 -0300

    Arrumando os erros na migration

---
+++ app/Http/Controllers/TccController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tcc;
+use App\Http\Requests\StoreTccRequest;
+use App\Http\Requests\UpdateTccRequest;
+
+class TccController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTccRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTccRequest $request, Tcc $tcc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tcc $tcc)
+    {
+        //
+    }
+}
---
+++ database/migrations/2026_03_24_232543_create_tccs_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tccs', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 100);
+            $table->smallInteger('paginas');
+            $table->date('data');
+            $table->timestamp('hora');
+            $table->string('aluno', 100);
+            $table->string('palavras_chave', 200);
+            $table->string('pdf', 100);
+            $table->foreignId('orientador')
+                ->constrained('bancas')
+                ->onDelete('cascade');
+            $table->foreignId('banca_1')
+                ->constrained('bancas')
+                ->onDelete('cascade');
+            $table->foreignId('banca_2')
+                ->constrained('bancas')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tccs');
+    }
+};
