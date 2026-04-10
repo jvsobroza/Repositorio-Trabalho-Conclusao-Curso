5c930eb82a99434c6cc27dc25cf744f278209b21
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-04-10 15:14:19 -0300

    Correção de bug no edit do tcc, que o formato estava inválido, criado método usando acessor do Laravel para pegar somente os 5 primeiros caracteres da hora, não dando erro no formato

--- app/Models/Tcc.php
+++ app/Models/Tcc.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tcc extends Model
 {
     protected $fillable = ['titulo', 'paginas', 'data', 'hora', 'aluno', 'resumo', 'palavras_chave', 'pdf', 'orientador', 'banca_1', 'banca_2'];
+    public function getHoraAttribute($value)
+    {
+        if ($value) {
+            return substr($value, 0, 5); //retorna HH:mm, sem erro no edit
+        }
+        return $value;
+    }
 
     //UM tcc tem MUITAS pessoas da banca
     public function banca1()
